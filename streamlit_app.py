d7d0a4775221044d2489c02618c57bcb1cd871c9
sunshinebeats <113069328+sunshinebeats789@users.noreply.github.com>
2022-10-12 18:22:52 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
-import pandas
+#import pandas
+#import requests
+import snowflake.connector
+from urllib.error import URLError
 
 
 streamlit.title('My Moms New Healthy Diner')
@@ -30,7 +33,7 @@ streamlit.header('Fruityvice Fruit Advice!')
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
 
-import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # write your own comment -what does the next line do? 
@@ -39,7 +42,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-import snowflake.connector
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("select * from fruit_load_list")
@@ -48,4 +51,8 @@ streamlit.header("the fruit load list contains:")
 streamlit.dataframe(my_data_rows)
 
 add_my_fruit = streamlit.text_input('What fruit would you like to add?','zzz')
-streamlit.write('Thanks for adding: ', add_my_fruit)
+streamlit.write('Thanks for adding ', add_my_fruit)
+
+#this will not work correctly, but just go with it for now
+
+my_cur.execute("insert into fruit_load_list values ('from streamlit')")
